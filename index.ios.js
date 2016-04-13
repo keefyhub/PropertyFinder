b144daaceedf8e97af32120084d3e7f3c89ef6fb
Keith Light <keith.light1986@gmail.com>
2016-04-13 09:31:14 +0100

    Add repo to github

---
+++ index.ios.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+'use strict';
+
+var React = require('react-native');
+var SearchPage = require('./SearchPage');
+
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+class PropertyFinder extends React.Component {
+  render() {
+    return (
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+React.AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
+
